79b551e22c94965563c572d843d093cd33b7e6e6
Sahana N <nsahana@Sahanas-MBP-2.bbrouter>
2022-11-08 19:14:55 +0530

    Create sample maven project to say hello

---
+++ src/main/java/com/demo/hellojenkins/HelloJenkinsApplication.java
@@ -0,0 +1,17 @@
+package com.demo.hellojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJenkinsApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJenkinsApplication.class, args);
+		System.out.println("Hello Jenkins");
+//        System.exit(0);
+
+	}
+
+}
